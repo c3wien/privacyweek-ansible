fcb3b54b451b4e4d580fc5eb6a1ba57dde615df9
Clemens Hopfer <spam-datacop@wireloss.net>
2021-10-16 21:23:54 +0200

    is it 2021 already?

--- roles/video/hdmi-desktop/files/display-xrandr.sh
+++ roles/video/hdmi-desktop/files/display-xrandr.sh
@@ -5,7 +5,13 @@
 # set link resolution
 for VAR in "$(xrandr | egrep "^HDMI.* connected" | awk '{print $1}')"; do
 	echo "setting output $VAR"
+{% if desktop_extend_virtual == true %}
 	xrandr --output $VAR --mode {{ desktop_resolution_mode }} --rate {{ desktop_resolution_rate }}
+	xrandr --addmode VIRTUAL1 {{ desktop_resolution_mode }}
+	xrandr --output VIRTUAL1 --primary --mode {{ desktop_resolution_mode }} --rate {{ desktop_resolution_rate }} --left-of $VAR
+{% else %}
+	xrandr --output $VAR --mode {{ desktop_resolution_mode }} --rate {{ desktop_resolution_rate }}
+{% endif %}
 done
 
 # disable link sleep
--- roles/video/hdmi-desktop/files/run-tigervnc.sh
+++ roles/video/hdmi-desktop/files/run-tigervnc.sh
@@ -4,12 +4,11 @@ if [ -z "$DISPLAY" ]; then
 	echo "display is not set, you have to run this from a desktop session"
 	exit 1
 fi
-
-# quick hack, let's make a systemd unit out of this
-/usr/share/novnc/utils/launch.sh --vnc "localhost:5900" --listen {{ desktop_novnc_port }} &
-
 while sleep 1; do
 	echo "staring server.."
-	x0tigervncserver -PasswordFile /home/debian/.vnc/passwd
+{% if desktop_vnc_password != '' %}
+	x0tigervncserver -Geometry {{ desktop_resolution_mode }} -PasswordFile /home/{{desktop_username}}/.vnc/passwd
+{% else %}
+	x0tigervncserver -Geometry {{ desktop_resolution_mode }} -SecurityTypes None,TLSNone
+{% endif %}
 done
-
